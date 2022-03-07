e5bd2021e9096a8dfdab1232e25df648cef8ab47
ancs214 <ancs214@gmail.com>
2022-03-07 18:52:30 -0500

    updated request logic with dynamic variables

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -1,8 +1,18 @@
-var getUserRepos = function () {
-    fetch("https://api.github.com/users/octocat/repos").then(function(response) {
+var getUserRepos = function (user) {
+    //format github api url
+    var apiUrl = "https://api.github.com/users/" + user + "/repos";
+    
+    //make a request to the url
+    fetch(apiUrl).then(function(response) {
     response.json().then(function(data) {
         console.log(data);
     })
 });
 };
-getUserRepos();
+getUserRepos("facebook");
+
+
+
+//then() method returns a promise
+//after fetching, response was formatted to JSON with json() method
+//function(data) is a callback function that captures tha actual data
